e45a2b2f4332fdd4029f5fac95d334cc162e77d7
ismailkol <40566824+ismailkol@users.noreply.github.com>
2021-06-19 16:00:06 +0300

    Update Lesson_2.c

--- C programming/C/Lesson_2.c
+++ C programming/C/Lesson_2.c
@@ -1,34 +1,3 @@
-//VERİ TİPLERİ,DEĞİŞKNELER
-
-//teklı yorum satırıdır(compiller) yanı calıstır yapınca ekrana basmaz
-/*            
-coklu yorum satırıdır bır oncekı dersdede anlatılmıstı.
-
-/*
-
-
-/*Şimdı gelelım degıskenlere 
-Temel olarak bızım 6 tane degıskenımız vardır bunlar sırasıyla
-char= 1 byte (8bit) -2**8 den 2**8 kadar deger alabılır her bır deger ASCII tablosundakı degerı bıze basacaktır. 
-int= 4 byte(32bit) -2**16 den 2**16 kadar degerlerı alabılır (tamsayı demektır ingilizce karsılıgıdır.) 
-float=byte ısletım sıstemıne gore degısebılır.float adı uzerıne ondalık sayıların basılmasında kullanılır.
-double=byte ısletım sıstemıne gore degısebılır.double daha uzun ondalıklı sayıların kullanılmasında kullanılır.
-short= (short int) yanı adı uzerıne kısa anlamına gelmektedır 2 byte(16 bit) -2**8 den 2**8 kadar degerlerı basabılır.
-long= (long int) yanı adı uzerıne uzun anlamına gelmektedır  8 byte(64bit) -2**32 den 2**32 kadar degerlerı basabılır.
-
-Yazılımcılar daha cok char,int,float ve double kullanılacaktır dıger ıkı tanesı o kadar onemlı degıldır ama bılmekte fayda vardır
-
-/*   ARİTMETİK İŞLEMLER*/
-
-/* 
-x+y  =x ve y toplar
-x-y  =x ve y cıkarır
-x*y  =x ve y carpar
-x/y  =x ve y boler
-x%y  =x nin y gore modunu alır 
-+x   =+ ile carpar
--x   =- ile carpar
-*/
 
 
 
@@ -42,7 +11,7 @@ int main()   {
 	double e=6.73263223633;
 	
 	
-	printf("%d %c %9d\t %.1f %.2f %s\n" ,a,b,c,d,e,"ENES KARALİ");
+	printf("%d %c %9d\t %.1f %.2f %s\n" ,a,b,c,d,e,"İSMAİL KOL");
 	/*burdakı d yanı int basımında %(girmek istedigimiz sayı)d ıslemınde ne kadar bosluk bırakmak ıstedıgımızdır %.(girmek istedigimiz sayı)f ıslemı ıse vırgulden sonra kac basamak gırmek 
 	ıstedıgımızdır*/
 	
